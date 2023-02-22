c310275872f44ab7d4399121cb9142d1de30bd39
Ardium <ardiums@gmail.com>
2023-02-22 17:54:37 +0900

    Revert "- 설정 화면 접근시 로그인한 유저의 정보를 조회 및 하위 컴포넌트에 데이터 전달하도록 구현"
    This reverts commit f8cb336712eaf96d9f05d27701717aa4c205c88f.

--- frontend/web-app-admin/src/views/admin-web/BaseAdminSettings.vue
+++ frontend/web-app-admin/src/views/admin-web/BaseAdminSettings.vue
@@ -1,12 +1,9 @@
 <template>
   <v-container>
-    <PrivateInformation
-      :mode="'Setting'"
-      :employee="this.employee"
-    ></PrivateInformation>
+    <PrivateInformation :mode="'Setting'"></PrivateInformation>
     <v-divider></v-divider>
 
-    <UserPassword :employee="this.employee"></UserPassword>
+    <UserPassword></UserPassword>
   </v-container>
 </template>
 
@@ -18,32 +15,6 @@ export default {
   components: {
     PrivateInformation,
     UserPassword,
-  },
-  data() {
-    return {
-      employee: {},
-    };
-  },
-  mounted: function () {
-    this.$nextTick(function () {
-      this.getUserInformation();
-    });
-  },
-  methods: {
-    getUserInformation: function () {
-      // TODO: employeeNo를 로그인한 사용자의 직원 번호로 교체 필요
-      let employeeNo = "123456";
-      let path = "/api/admin-web/user/" + employeeNo;
-
-      this.$axios
-        .get(path)
-        .then((response) => {
-          this.employee = response.data;
-        })
-        .catch(function (error) {
-          console.log("[ERR/getUserInformation]" + error);
-        });
-    },
   },
 };
 </script>
